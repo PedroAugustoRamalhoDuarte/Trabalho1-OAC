4af94da4ef0f9f3c079c837e17ee145badae75f3
Waliff Cordeiro <waliff.cordeiro@gmail.com>
2019-09-12 19:48:55 -0300

    refatoração lenaeye

--- convert_negative.asm
+++ convert_negative.asm
@@ -8,29 +8,27 @@
 		lw 	a1, address
 	
 	#-------------------------------------------------------------------------
-	# Funcao convert_negative: Recebe de parametro o endereço de inicio de uma imagem
-	# pega 
+	# Funcao convert_negative: Subtrair da máscara 0x00FFFFFF cada pixel
 	#
 	# A função foi implementada da seguinte maneira:
-	# 1 - Coloca o ponteiro no inicio da imagem
-	# 2 - Pega o valor da word
-	# 3 - Subtrai a máscara 0x00FFFFFF da word
+	# 1 - Coloca o ponteiro no endereço do inicio da imagem
+	# 2 - Pega o valor da word atual
+	# 3 - Subtrai da máscara 0x00FFFFFF o valor da word
 	# 4 - Devolve o novo valor/cor para a imagem/endereço
-	# 5 - Avança o ponteiro do endereço
+	# 5 - Avança o ponteiro do endereço para a próxima word
 	# 6 - Repete do passo 2 ao 4 de acordo com o size (tamanho da imagem)
 	#-------------------------------------------------------------------------
 	
 	convert_negative:
-		# define parâmetros e segue para a função para carregar a imagem
-		
-		li 	s7, 0x00FFFFFF			# máscara que representa a constante(255) subtraida de cada informação de cor
+		# define parâmetros e segue para a função para carregar a imagem		
+		li 	s7, 0x00FFFFFF			# máscara que representa a constante 255 em cada byte com informação de cor
 		li 	s8, 4096 			# Tamanho da imagem (words) - Quantidade de loops
 		b	convert_negative_loop		# após inicialização, avança para a subrotina do loop		
 		
 	convert_negative_loop:
 		
-		lw 	s0,0(a1)			# pega o valor da imagem em 1 words
-		sub	s0,s7,s0			# subtrair byte a byte o valor de 255
+		lw 	s0,0(a1)			# pega o valor de uma word da imagem
+		sub	s0,s7,s0			# subtrair byte a byte do valor 255 (255 - Informação de cor)
 		sw 	s0,0(a1)			# devolve 255 subtraido do valor para a imagem/endereço		
 		addi 	a1,a1,4				# pega a próxima word da imagem				
 		blez 	s8, cabouImagem			# verifica se o contador é 0 para saber se chegamos no fim da imagem
@@ -38,6 +36,6 @@
 		b 	convert_negative_loop 		# repete o procedimento se não tiver chegado ao fim	
 		
 	cabouImagem:
-		ret
+		ret					# retorna pra rotina principal
 		
 	.include "/home/waliffcordeiro/UnB/OAC/Trabalho1-OAC/load_image.asm"
--- convert_redtones.asm
+++ convert_redtones.asm
@@ -11,12 +11,11 @@
 		lw 	a1, address
 		
 	#-------------------------------------------------------------------------
-	# Funcao convert_redtones: Recebe de parametro o endereço de inicio de uma imagem
-	# aplica uma máscara que zera todos os bits que não possuem a informação do vermelho
+	# Funcao convert_redtones: Aplica uma máscara que zera todos os bits que não possuem a informação do vermelho
 	#
 	# A função foi implementada da seguinte maneira:
 	# 1 - Coloca o ponteiro no endereço do inicio da imagem
-	# 2 - Carrega o valor da word
+	# 2 - Pega o valor da word atual
 	# 3 - Aplica a máscara 0x00FF0000 na word
 	# 4 - Devolve o novo valor/cor para o endereço/imagem
 	# 5 - Repete do passo 2 ao 4 de acordo com o size (tamanho da imagem)
@@ -26,14 +25,13 @@
 			
 		lw 	s0,0(a1)			# pega o valor da imagem em 1 word
 		and	s0,s7,s0			# aplica a máscara
-		sw 	s0,0(a1)			# devolve o valor do word para o destino no endereço
-		addi	a1,a1,4				# aponta para a próxima word da imagem
-		
-		blez 	s8, cabouImagem			# se a quantidade de repetições = 0, encerramos a execução
+		sw 	s0,0(a1)			# devolve o valor/cor da word para o endereço/imagem
+		addi	a1,a1,4				# aponta para a próxima word da imagem		
+		blez 	s8, cabouImagem			# verifica se o contador s8 chegou em 0 para encerrar o programa
 		addi 	s8, s8, -1			# decrementa o contador 
 		b 	convert_redtones		# continua enquanto não acabar a imagem
 	
 	cabouImagem:
-		ret
+		ret					# retorna pra rotina principal
 				
 	.include "/home/waliffcordeiro/UnB/OAC/Trabalho1-OAC/load_image.asm"
