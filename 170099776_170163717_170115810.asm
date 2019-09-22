973d3ef5f4348c97a20f2f5f7bce0342e75c9551
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2019-09-22 15:49:21 -0300

    Merge convert redtones

--- 170099776_170163717_170115810.asm
+++ 170099776_170163717_170115810.asm
@@ -219,6 +219,15 @@ Negativo:
 		b	Menu
 
 Vermelho:
+		# Local que está sendo carregada a imagem
+		la 	a0, image_name
+		lw 	a1, address
+		la 	a2, buffer
+		lw 	a3, size			
+		jal 	load_image
+		lw 	a1, address
+		
+		jal	convert_redtones
 		b	Menu
 
 LoadImage:
@@ -621,6 +630,31 @@ Sair:
 		
 	cabouImagem:
 		ret					# retorna pra rotina principal
+		
+	#-------------------------------------------------------------------------
+	# Funcao convert_redtones: Aplica uma máscara que zera todos os bits que não possuem a informação do vermelho
+	#
+	# A função foi implementada da seguinte maneira:
+	# 1 - Coloca o ponteiro no endereço do inicio da imagem
+	# 2 - Pega o valor da word atual
+	# 3 - Aplica a máscara 0x00FF0000 na word
+	# 4 - Devolve o novo valor/cor para o endereço/imagem
+	# 5 - Repete do passo 2 ao 4 de acordo com o size (tamanho da imagem)
+	#-------------------------------------------------------------------------
+		
+	convert_redtones:
+		# Máscara e tamanho da imagem - Quando declarei no .data deu ruim
+		li 	s8, 4096			# Tamanho da imagem - Quantidade de words que serão acessadas
+		li 	s7, 0x00FF0000			# Máscara que mantém apenas a informação do vermelho (RGB)
+	
+	convert_redtones_loop:	
+		lw 	s0,0(a1)			# pega o valor da imagem em 1 word
+		and	s0,s7,s0			# aplica a máscara
+		sw 	s0,0(a1)			# devolve o valor/cor da word para o endereço/imagem
+		addi	a1,a1,4				# aponta para a próxima word da imagem		
+		blez 	s8, cabouImagem			# verifica se o contador s8 chegou em 0 para encerrar o programa
+		addi 	s8, s8, -1			# decrementa o contador 
+		b 	convert_redtones_loop		# continua enquanto não acabar a imagem
 #------------------------------------------------------------------------
 
 #
